be09d9a7998afc66b45a5b46bebc83fd1dfcbc1c
dezebbo <131293136+dezebbo@users.noreply.github.com>
2023-06-04 21:42:28 +0200

    export db into module db_model.py

--- 03_application-team/app.py
+++ 03_application-team/app.py
@@ -2,35 +2,14 @@ from flask import Flask, Response, url_for, request, session, abort, render_temp
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from funcs import *
-
+from db_model import *
 
 # default config
 app = Flask(__name__)
 app.secret_key = "~((<SH,jM_YU9_x3$2f!_x2"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xqrornwg:QjZBbShdIqvLjHohXXMfvIXoSuc3lnZr@horton.db.elephantsql.com/xqrornwg'
-db = SQLAlchemy(app)
-# db.init_app(app)
-
-
-class Accounts(db.Model):
-    """
-    represents the table structure of the PostgreSQL server
-    *DOES NOT create a new if table, if non-existent*
-    """
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-
-
-# postgreSQL DB config coming soon
-patientData = [
-    {"id": 1, "name": "John", "age": 35, "weight": 75.5, "sex": "", "symptoms": []},
-    {"id": 2, "name": "Sarah", "age": 42, "weight": 68.2, "sex": "", "symptoms": []},
-    {"id": 3, "name": "Tamer", "age": 22, "weight": 85.2, "sex": "", "symptoms": []},
-    {"id": 4, "name": "Noah", "age": 23, "weight": 70.2, "sex": "", "symptoms": []},
-    {"id": 5, "name": "Mike", "age": 54, "weight": 80.1, "sex": "", "symptoms": []}
-]
+db.init_app(app)
 
 # flask-login config
 login_manager = LoginManager()
@@ -39,7 +18,7 @@ login_manager.login_view = "login"
 
 
 # user config
-class User (UserMixin):
+class User(UserMixin):
     pass
 
 
@@ -75,15 +54,15 @@ def assignTokens(id):
         parsedOutput = "Error"
     if "Sign_symptom" in parsedOutput:
         print(parsedOutput["Sign_symptom"])
-        patientData[id-1]["symptoms"].append(parsedOutput["Sign_symptom"])
-    return render_template("patientSpec.html", patientData=patientData[id-1])
+        patientData[id - 1]["symptoms"].append(parsedOutput["Sign_symptom"])
+    return render_template("patientSpec.html", patientData=patientData[id - 1])
 
 
 @app.route("/patients/<int:id>")
 @login_required
 def patients_route(id):
     print("You pressed on: " + str(id))
-    return render_template("patientSpec.html", patientData=patientData[id-1])
+    return render_template("patientSpec.html", patientData=patientData[id - 1])
 
 
 @app.route("/sendInput", methods=["POST"])
