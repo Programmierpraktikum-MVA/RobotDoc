6cd4e92c4ce8ed48c5fdfae9f837442cce7010f7
tameraktasTU <tamer.aktas@web.de>
2023-06-06 17:29:26 +0200

    implement alternative folder structure

---
+++ 03_application-team/blueprints/auth/auth.py
@@ -0,0 +1,64 @@
+from flask import Flask, Response, url_for, request, session, abort, render_template, redirect, jsonify, Blueprint
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from util.db_model import *
+
+auth = Blueprint('auth', __name__)
+
+# flask-login config
+login_manager = LoginManager()
+
+
+class User (UserMixin):
+    pass
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect("/home")
+    if request.method == "POST":
+        username = request.form["username"]
+        remember_me = request.form.get("remember_me")
+        username_db = None
+        user_data = db.session.scalars(
+            db.select(Accounts).filter_by(username=username))
+        for row in user_data:
+            username_db = row.username
+            password_db = row.password
+        if username_db == None:
+            return render_template("index.html", loginFailed=True)
+        password_cand = request.form["password"]
+        if username == username_db and password_cand == password_db:
+            user = User()
+            user.id = username
+            if (remember_me):
+                login_user(user, remember=True)
+            else:
+                login_user(user)
+            return redirect("/home")
+        return render_template("index.html", loginFailed=True)
+    else:
+        return render_template("index.html")
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return redirect("/login")
+
+
+@login_manager.user_loader
+def load_user(username):
+    username_db = db.session.scalars(
+        db.select(Accounts.username).filter_by(username=username))
+    if username_db == None:
+        return
+    user = User()
+    user.id = username
+    return user
---
+++ 03_application-team/util/funcs.py
@@ -0,0 +1,36 @@
+import requests
+
+# huggingface api (reference: https://huggingface.co/d4data/biomedical-ner-all)
+API_URL = "https://api-inference.huggingface.co/models/d4data/biomedical-ner-all"
+headers = {"Authorization": "Bearer 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"}
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+def convertString(data):
+    output = []
+    for d in data:
+        score_pct = round(d['score'] * 100, 2)
+        line = f"{d['entity_group']}: {d['word']} (score: {score_pct}%, start: {d['start']}, end: {d['end']})"
+        output.append(line)
+    output.append("")
+    return output
+
+
+def parseString(data):
+    # Create a dictionary to hold the arrays
+    entity_groups = {}
+    # Iterate over each item in the list
+    for item in data:
+        entity_group = item['entity_group']
+        word = item['word']
+    # If the entity group already exists, append the word to the existing array
+        if entity_group in entity_groups:
+            entity_groups[entity_group].append(word)
+    # Otherwise, create a new array for the entity group
+        else:
+            entity_groups[entity_group] = [word]
+    return entity_groups
