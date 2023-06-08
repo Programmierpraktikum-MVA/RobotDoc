07f035245ba3655377fa9370cb2b20183b72ed88
dezebbo <131293136+dezebbo@users.noreply.github.com>
2023-06-08 21:32:02 +0200

    implement pw hashing

--- 03_application-team/util/db_model.py
+++ 03_application-team/util/db_model.py
@@ -11,6 +11,7 @@ class Accounts(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     password = db.Column(db.String)
+    hash = db.Column(db.LargeBinary)
 
 
 # postgreSQL DB config coming soon
