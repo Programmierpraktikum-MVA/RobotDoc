3c5045e08a7aebe14e4a95628427c9e574371aec
dezebbo <131293136+dezebbo@users.noreply.github.com>
2023-06-11 22:26:10 +0200

    db access to sign up user

---
+++ 03_application-team/util/db_access.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from util.db_model import *
+import bcrypt as bcr
+
+
+def register_user(username_cand, password):
+    """
+    Registers a new user with given username and password.
+    Raises exception if username is already in use.
+    Returns 0 on success.
+    """
+    username_db = db.session.scalars(
+        db.select(Accounts.username).filter_by(username=username))
+    if username_db is not None:
+        raise Exception
+
+    pw = password.encode('UTF-8')
+    salt = bcr.gensalt()
+    pw = bcr.hashpw(pw, salt)
+
+    new_user = Accounts()
+    new_user.username = username_cand
+    new_user.hash = pw
+
+    db.session.add(new_user)
+    db.session.commit()
+    return 0
