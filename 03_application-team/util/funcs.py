e251f66de52e59c4ee235fb689f00b2ecf9668f6
yungguerra <simonguerra@outlook.de>
2024-07-03 16:56:48 +0200

    deleted un used  code

--- 03_application-team/util/funcs.py
+++ 03_application-team/util/funcs.py
@@ -1,73 +1,13 @@
 import requests
 from util.exceptions import *
-#import modules.model.model as ml
 from util.db_model import *
 from enum import Enum
-#DENEME IMPORTLAR
-#from util.knowledge_graph import *
+
 import matplotlib
 
 # huggingface api (reference: https://huggingface.co/d4data/biomedical-ner-all)
 API_URL = "https://api-inference.huggingface.co/models/d4data/biomedical-ner-all"
 headers = {"Authorization": "Bearer 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"}
-
-# available natural language models
-class NLP(Enum):
-    HF = 1
-    MLTEAM = 2
-
-# available prediction models
-class PM(Enum):
-    MLTEAM = 1
-
-#DENEME: GRAPH ÇİZİMİ BİTMEDİ!!
-
-def drawGraph(input):
-
-    return 0
-
-def getSymptoms(input, model):
-    if model == NLP.HF:   
-        payload = {
-            "inputs": input
-            } # get correct format
-        response = convertString(requests.post(API_URL, headers=headers, json=payload).json) # api call
-        if "Sign_symptom" in response: return response["Sign_symptom"]
-        
-    if model == NLP.MLTEAM:
-        return ml.process_input(input) # get symptoms (NLP)
-    else: raise InvalidModelError
-
-def getDiagnosis(symptoms, model,threshold):
-    if model == PM.MLTEAM:
-        # NOTE: symptoms need to be in the format: {"symptoms":[]}
-        return ml.predict(symptoms,threshold) # get diagnosis (prediction)
-    else: raise InvalidModelError
-
-def convertString(data):
-    output = []
-    for d in data:
-        score_pct = round(d['score'] * 100, 2)
-        line = f"{d['entity_group']}: {d['word']} (score: {score_pct}%, start: {d['start']}, end: {d['end']})"
-        output.append(line)
-    output.append("")
-    return output
-
-
-def parseString(data):
-    # Create a dictionary to hold the arrays
-    entity_groups = {}
-    # Iterate over each item in the list
-    for item in data:
-        entity_group = item['entity_group']
-        word = item['word']
-        # If the entity group already exists, append the word to the existing array
-        if entity_group in entity_groups:
-            entity_groups[entity_group].append(word)
-        # Otherwise, create a new array for the entity group
-        else:
-            entity_groups[entity_group] = [word]
-    return entity_groups
 
 
 def validate_username(username):
